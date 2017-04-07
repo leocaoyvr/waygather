aef72eb3b002dc8e2bf3393008339dda94ff2dbe
Leo Cao <leo@ilite.me>
2017-04-07 06:55:01 -0700

    add select category modal

--- dev/script/ux.js
+++ dev/script/ux.js
@@ -3,6 +3,15 @@
 /* for user interactions
 /* ========================= */
 
+/* Click to Hide this Item
+/* ------------------------- */
+
+$(function() {
+  $('.js-click-to-hide').click(function() {
+    $(this).hide();
+  });
+});
+
 /* Click to Show Target
 /* ------------------------- */
 
@@ -10,7 +19,36 @@ $(function() {
   $('.js-show-target').click(function(){
     var tar = $(this).attr('target');
 
-    $(this).hide();
     $('.' + tar).addClass('active');
+    return false;
+  });
+});
+
+/* Click to Switch Among Tabs
+/* ------------------------- */
+
+$(function() {
+  $('.js-tab-hd').click(function(){
+    if(!$(this).hasClass('active')) {
+      var tabTar = '.' + $(this).attr('target');
+      var tabParent = $(this).parents('.js-tab-parent');
+
+      console.log(tabTar);
+
+      $(tabParent).find('.js-tab-hd').removeClass('active');
+      $(this).addClass('active');
+
+      $(tabParent).find('.js-tab-bd').removeClass('active');
+      $(tabParent).find(tabTar).addClass('active');
+    }
+  });
+});
+
+/* Click to Close Modal
+/* ------------------------- */
+
+$(function() {
+  $('.js-modal-close').click(function() {
+    $(this).parents('.modal-outer').removeClass('active');
   });
 });
